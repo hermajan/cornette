b392a5512c73819f2b99ceb721899c91036109d6
Jan Hermann <hermajan@gmail.com>
2023-08-29 13:42:26 +0200

    Added responses

---
+++ src/Responses/PngResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Cornette\Responses;
+
+use Nette\Application\Response;
+use Nette\Application\UI\Template;
+use Nette\Http\{IRequest as IHttpRequest, IResponse as IHttpResponse};
+
+class PngResponse implements Response {
+	/** @var string */
+	private $source;
+	
+	public function __construct(string $source) {
+		$this->source = $source;
+	}
+	
+	public function getSource(): string {
+		return $this->source;
+	}
+	
+	public function send(IHttpRequest $httpRequest, IHttpResponse $httpResponse): void {
+		$httpResponse->setContentType("image/png", "utf-8");
+		
+		if($this->source instanceof Template) {
+			$this->source->render();
+		} else {
+			echo $this->source;
+		}
+	}
+}
